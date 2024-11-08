a4105f551b16ec38590058a4e7b505f1f19cac46
foko <qkrrudgh0@naver.com>
2024-11-08 13:06:43 +0900

    test:TimeLine 테스트항목을 추가함

--- src/test/kotlin/rkrk/reservation/unit/TimeLineTest.kt
+++ src/test/kotlin/rkrk/reservation/unit/TimeLineTest.kt
@@ -7,15 +7,11 @@ import rkrk.reservation.warehouse.reservation.domain.ReservationTime
 import rkrk.reservation.warehouse.reservation.domain.TimeLine
 import java.time.LocalDateTime
 
-// 앞쪽으로 겹치는
-// 빈타임라인에 추가
-// 포개지는
-// 연도
 class TimeLineTest {
     @Test
-    @DisplayName("타임라인에 겹치지않는 예약을 더할수있다")
-    fun addReservationTime() {
-        val timeLine = createTimeLine()
+    @DisplayName("빈타임라인에 예약을 더할수있다")
+    fun addReservationTimeToBlankTimeLine() {
+        val timeLine = TimeLine()
         val targetTime =
             ReservationTime(
                 LocalDateTime.of(2024, 10, 24, 10, 30, 0),
@@ -24,14 +20,78 @@ class TimeLineTest {
 
         timeLine.addTime(targetTime)
 
-        Assertions.assertThat(timeLine.reservationTimes.size).isEqualTo(4)
+        Assertions.assertThat(timeLine.reservationTimes.size).isEqualTo(1)
+        Assertions.assertThat(timeLine.reservationTimes[0]).isEqualTo(targetTime)
+    }
+
+    @Test
+    @DisplayName("타임라인에 겹치지않는 예약을 더할수있다")
+    fun addReservationTime() {
+        val timeLine = createBasicTimeLine()
+        val targetTime =
+            ReservationTime(
+                LocalDateTime.of(2024, 10, 24, 10, 30, 0),
+                LocalDateTime.of(2024, 10, 25, 9, 30, 0),
+            )
+
+        timeLine.addTime(targetTime)
+
+        Assertions.assertThat(timeLine.reservationTimes.size).isEqualTo(5)
         Assertions.assertThat(timeLine.reservationTimes[1]).isEqualTo(targetTime)
     }
 
     @Test
+    @DisplayName("타임라인의 제일 왼쪽에 예약을 더할수있다")
+    fun leftAddReservationTime() {
+        val timeLine = createBasicTimeLine()
+        val targetTime =
+            ReservationTime(
+                LocalDateTime.of(2024, 10, 10, 10, 30, 0),
+                LocalDateTime.of(2024, 10, 11, 9, 30, 0),
+            )
+
+        timeLine.addTime(targetTime)
+
+        Assertions.assertThat(timeLine.reservationTimes.size).isEqualTo(5)
+        Assertions.assertThat(timeLine.reservationTimes[0]).isEqualTo(targetTime)
+    }
+
+    @Test
+    @DisplayName("타임라인의 제일 오른쪽에 예약을 더할수있다")
+    fun rightAddReservationTime() {
+        val timeLine = createBasicTimeLine()
+        val targetTime =
+            ReservationTime(
+                LocalDateTime.of(2025, 11, 10, 10, 30, 0),
+                LocalDateTime.of(2025, 11, 11, 9, 30, 0),
+            )
+
+        timeLine.addTime(targetTime)
+
+        Assertions.assertThat(timeLine.reservationTimes.size).isEqualTo(5)
+        Assertions.assertThat(timeLine.reservationTimes[4]).isEqualTo(targetTime)
+    }
+
+    @Test
+    @DisplayName("타임라인에 시작시간과 종료시간의 연도가 다른 예약도 더할수있다")
+    fun otherYearAddReservationTime() {
+        val timeLine = createBasicTimeLine()
+        val targetTime =
+            ReservationTime(
+                LocalDateTime.of(2023, 12, 28, 10, 30, 0),
+                LocalDateTime.of(2024, 1, 20, 9, 30, 0),
+            )
+
+        timeLine.addTime(targetTime)
+
+        Assertions.assertThat(timeLine.reservationTimes.size).isEqualTo(5)
+        Assertions.assertThat(timeLine.reservationTimes[0]).isEqualTo(targetTime)
+    }
+
+    @Test
     @DisplayName("타임라인에 뒤쪽으로 겹치는 예약을 추가하려고 하면 예외가 발생한다")
-    fun addReservationTimeThrowException() {
-        val timeLine = createTimeLine()
+    fun rearOverlapReservationTimeThrowException() {
+        val timeLine = createBasicTimeLine()
         val targetTime =
             ReservationTime(
                 LocalDateTime.of(2024, 10, 24, 10, 30, 0),
@@ -43,7 +103,52 @@ class TimeLineTest {
             .isInstanceOf(IllegalStateException::class.java)
     }
 
-    private fun createTimeLine(): TimeLine {
+    @Test
+    @DisplayName("타임라인에 앞쪽으로 겹치는 예약을 추가하려고 하면 예외가 발생한다")
+    fun frontOverlapReservationTimeThrowException() {
+        val timeLine = createBasicTimeLine()
+        val targetTime =
+            ReservationTime(
+                LocalDateTime.of(2024, 10, 27, 10, 30, 0),
+                LocalDateTime.of(2024, 10, 29, 11, 30, 0),
+            )
+
+        Assertions
+            .assertThatThrownBy { timeLine.addTime(targetTime) }
+            .isInstanceOf(IllegalStateException::class.java)
+    }
+
+    @Test
+    @DisplayName("타임라인에 완전히 겹치는 예약을 추가하려고 하면 예외가 발생한다")
+    fun equalOverlapReservationTimeThrowException() {
+        val timeLine = createBasicTimeLine()
+        val targetTime =
+            ReservationTime(
+                LocalDateTime.of(2024, 10, 25, 10, 30, 0),
+                LocalDateTime.of(2024, 10, 27, 13, 30, 0),
+            )
+
+        Assertions
+            .assertThatThrownBy { timeLine.addTime(targetTime) }
+            .isInstanceOf(IllegalStateException::class.java)
+    }
+
+    @Test
+    @DisplayName("타임라인에 있는 예약에 포개지는 예약을 추가하려고 하면 예외가 발생한다")
+    fun includeOverlapReservationTimeThrowException() {
+        val timeLine = createBasicTimeLine()
+        val targetTime =
+            ReservationTime(
+                LocalDateTime.of(2024, 10, 26, 10, 30, 0),
+                LocalDateTime.of(2024, 10, 26, 13, 30, 0),
+            )
+
+        Assertions
+            .assertThatThrownBy { timeLine.addTime(targetTime) }
+            .isInstanceOf(IllegalStateException::class.java)
+    }
+
+    private fun createBasicTimeLine(): TimeLine {
         val timeLine = TimeLine()
 
         val reservationTime1 =
@@ -61,9 +166,15 @@ class TimeLineTest {
                 LocalDateTime.of(2024, 10, 28, 10, 30, 0),
                 LocalDateTime.of(2024, 10, 29, 13, 30, 0),
             )
+        val reservationTime4 =
+            ReservationTime(
+                LocalDateTime.of(2024, 12, 28, 10, 30, 0),
+                LocalDateTime.of(2025, 1, 10, 13, 30, 0),
+            )
         timeLine.reservationTimes.add(reservationTime1)
         timeLine.reservationTimes.add(reservationTime2)
         timeLine.reservationTimes.add(reservationTime3)
+        timeLine.reservationTimes.add(reservationTime4)
 
         return timeLine
     }
