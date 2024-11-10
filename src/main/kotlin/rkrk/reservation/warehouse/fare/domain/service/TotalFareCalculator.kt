bcd1a17ea920a637c27e5f0c230b35638ada0806
foko <qkrrudgh0@naver.com>
2024-11-10 17:19:33 +0900

    refactor:FareCalculator을 TotalFareCalculator내부로 넣음

--- src/main/kotlin/rkrk/reservation/warehouse/fare/domain/service/TotalFareCalculator.kt
+++ src/main/kotlin/rkrk/reservation/warehouse/fare/domain/service/TotalFareCalculator.kt
@@ -1,34 +1,25 @@
 package rkrk.reservation.warehouse.fare.domain.service
 
+import rkrk.reservation.warehouse.fare.domain.fare.Fare
 import rkrk.reservation.warehouse.fare.domain.fare.NightSurcharge
 import rkrk.reservation.warehouse.fare.domain.fare.RegularFare
 import rkrk.reservation.warehouse.fare.domain.fare.WeekendSurcharge
 import rkrk.reservation.warehouse.fare.domain.timeperiod.NightTimePeriodStrategy
 import rkrk.reservation.warehouse.fare.domain.timeperiod.RegularTimePeriodStrategy
+import rkrk.reservation.warehouse.fare.domain.timeperiod.TimePeriodStrategy
 import rkrk.reservation.warehouse.fare.domain.timeperiod.WeekendTimePeriodStrategy
 import rkrk.reservation.warehouse.reservation.domain.ReservationTime
+import rkrk.reservation.warehouse.reservation.domain.TimeSlot
 import java.math.BigDecimal
 
 // ReservationTime을 받아서,해당 클래스의 날짜 금액을 계산해주는 도메인서비스
-// 날짜기반은 얘로 처리한다고 치고,쿠폰같은건 다른클래스에서 처리
-
-// 시작전 일요일체크
-
-// 엔드타임이 저녁이전이면 시작~엔드 +일반요금
-// 시작타임이 저녁이전이고 엔드타임이 저녁 이후면 시작~저녁(일반요금) + 저녁~엔드(저녁요금)
-// 시작타임이 저녁이후면 시작~엔드+ 저녁요금
-// TODO
-// fare와 TimePeriodStrategy를 둘다 di받고,그걸가지고 계산해서 리턴
-// 각 계산단위는 TimeSlot
-// night와 regular을 각각 계산
-// weekend도 그냥 주말이면 다시 0.2만큼을 추가로 더하고,아니면 0을 더함
+// 날짜기반은 얘로 처리하고,쿠폰같은건 여기서 계산값을 받아간후에 다른클래스에서 처리
 
 class TotalFareCalculator {
     fun calTotalFare(
         reservationTime: ReservationTime,
         price: Long,
     ): BigDecimal {
-        // 계산
         var totalPrice = BigDecimal(0)
         val timeSlots = reservationTime.toTimeSlots()
         val fareCalculators = createFareCalculators()
@@ -43,9 +34,21 @@ class TotalFareCalculator {
 
     private fun createFareCalculators(): List<FareCalculator> {
         val fareCalculators = mutableListOf<FareCalculator>()
+
         fareCalculators.add(FareCalculator(RegularFare(), RegularTimePeriodStrategy()))
         fareCalculators.add(FareCalculator(NightSurcharge(), NightTimePeriodStrategy()))
         fareCalculators.add(FareCalculator(WeekendSurcharge(), WeekendTimePeriodStrategy()))
+
         return fareCalculators
+    }
+
+    private class FareCalculator(
+        private val fare: Fare,
+        private val timePeriodStrategy: TimePeriodStrategy,
+    ) {
+        fun calculate(
+            timeSlot: TimeSlot,
+            baseMinuteRate: Long,
+        ): BigDecimal = fare.calFare(timePeriodStrategy.calTotalTime(timeSlot), baseMinuteRate)
     }
 }
